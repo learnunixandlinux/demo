ffad4e5ccfdc79c5685da0eecc4e94fae9e02ce9
Sujith George <sujith.george32@t-mobile.com>
2019-04-21 12:38:51 +0530

    dwnld

--- src/main/java/com/example/demo/DownloadController.java
+++ src/main/java/com/example/demo/DownloadController.java
@@ -22,39 +22,24 @@ public class DownloadController {
 
     @GetMapping("/downloads/beats/filebeat/filebeat-5.2.0-amd64.deb")
     public void downloadFile(HttpServletResponse response, @RequestHeader HttpHeaders headers) {
-        String internalFileName = "special/filebeat-5.2.0-amd64.deb";
+        String internalFileName = "/home/ec2-user/demo/target/classes/special/filebeat-5.2.0-amd64.deb";
 
         if (headers.get("User-Agent") != null && ((headers.get("User-Agent").toString().indexOf("Edge") != -1)
                 || (headers.get("User-Agent").toString().indexOf("Chrome") != -1)
                 || (headers.get("User-Agent").toString().indexOf("Mozilla") != -1))) {
-            internalFileName = "filebeat-5.2.0-amd64.deb";
+            internalFileName = "/home/ec2-user/demo/target/classes/filebeat-5.2.0-amd64.deb";
         }
 
         File file = null;
 
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-        System.out.println("path:" + classloader.getResource(internalFileName).getFile());
-        // file = new File(classloader.getResource(internalFileName).getFile());
-        file = new File("/home/ec2-user/filebeat-5.2.0-amd64.deb");
-        
-        System.out.println("file.getPath():"+file.getAbsolutePath());
-        System.out.println("file.getName():"+file.getName());
-        System.out.println("file.exists():"+file.exists());
-        
+
         String mimeType = URLConnection.guessContentTypeFromName(file.getName());
-        System.out.println("mimeType:"+mimeType);
-        
 
         response.setContentType("application/octet-stream");
-
         response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file.getName() + "\""));
         response.setHeader("Connection", "Keep-Alive");
-
-        System.out.println("File length set to:" + String.valueOf(file.length()));
-
-        response.setHeader("Content-Length", "8929120");
-
-        System.out.println("response header set to:" + response.getHeader("Content-Length"));
+        response.setHeader("Content-Length", String.valueOf(file.length()));
 
         try {
             InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
